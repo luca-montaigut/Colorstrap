8317a1b1677e0e00126b4653e7e3993cb7d70b65
Luca Montaigut <lmontaigut@gmail.com>
2020-03-12 12:00:41 +0100

    MVP ok

--- app/controllers/colorstraps_controller.rb
+++ app/controllers/colorstraps_controller.rb
@@ -1,9 +1,18 @@
 class ColorstrapsController < ApplicationController
+  def index
+
+  end
+  
   def show
-    
+    @color = Colorstrap.find(params[:id])
   end
 
   def create
+    @color = Colorstrap.create(primary: params[:primary], secondary: params[:secondary], success: params[:success], info: params[:info], warning: params[:warning], danger: params[:danger])
+    redirect_to @color
+  end
+
+  def update
     
   end
 end
