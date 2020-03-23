d0aaf7e016ae9d666c7843a1f4be7db2a405d66a
Luca Montaigut <lmontaigut@gmail.com>
2020-03-23 21:55:19 +0100

    fix some issues

--- app/controllers/colorstraps_controller.rb
+++ app/controllers/colorstraps_controller.rb
@@ -12,26 +12,17 @@ class ColorstrapsController < ApplicationController
   end
 
   def create
-    @color = Colorstrap.create(
-      primary: params[:primary], 
-      secondary: params[:secondary], 
-      success: params[:success], 
-      info: params[:info], 
-      warning: params[:warning], 
-      danger: params[:danger],
-      background: params[:background],
-      text_light: params[:text_light],
-      text_dark: params[:text_dark],
-      light: params[:light],
-      dark: params[:dark],
-      link_hover: params[:link_hover],
-      form_background: params[:form_background],
-      inner_background: params[:inner_background]
-    )
+    @color = Colorstrap.create(colorstrap_params)
     redirect_to @color
   end
 
   def update
     
+  end
+
+  private
+
+  def colorstrap_params
+    params.permit(:primary, :secondary, :success, :info, :warning, :danger, :background, :text_light, :text_dark, :light, :dark, :link_hover, :form_background, :inner_background)
   end
 end
--- app/models/colorstrap.rb
+++ app/models/colorstrap.rb
@@ -1,2 +1,16 @@
 class Colorstrap < ApplicationRecord
+  validates_format_of :primary, with: /\A#?(?:[A-F0-9]{3}){1,2}\z/i   
+  validates_format_of :secondary, with: /\A#?(?:[A-F0-9]{3}){1,2}\z/i   
+  validates_format_of :success, with: /\A#?(?:[A-F0-9]{3}){1,2}\z/i   
+  validates_format_of :info, with: /\A#?(?:[A-F0-9]{3}){1,2}\z/i   
+  validates_format_of :warning, with: /\A#?(?:[A-F0-9]{3}){1,2}\z/i   
+  validates_format_of :danger, with: /\A#?(?:[A-F0-9]{3}){1,2}\z/i  
+  validates_format_of :background, with: /\A#?(?:[A-F0-9]{3}){1,2}\z/i  
+  validates_format_of :text_light, with: /\A#?(?:[A-F0-9]{3}){1,2}\z/i  
+  validates_format_of :text_dark, with: /\A#?(?:[A-F0-9]{3}){1,2}\z/i  
+  validates_format_of :light, with: /\A#?(?:[A-F0-9]{3}){1,2}\z/i  
+  validates_format_of :dark, with: /\A#?(?:[A-F0-9]{3}){1,2}\z/i  
+  validates_format_of :link_hover, with: /\A#?(?:[A-F0-9]{3}){1,2}\z/i  
+  validates_format_of :form_background, with: /\A#?(?:[A-F0-9]{3}){1,2}\z/i  
+  validates_format_of :inner_background, with: /\A#?(?:[A-F0-9]{3}){1,2}\z/i  
 end
